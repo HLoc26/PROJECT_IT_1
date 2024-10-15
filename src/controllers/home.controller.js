d0d59e3549ea9eca91d01d895228820b258538bc
HLoc26 <dhl26052004@gmail.com>
2024-10-15 23:30:07 +0700

    Add rote to login.ejs and POST router for register and login

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,4 +1,8 @@
 // Các hàm trả về dữ liệu cho routes/web.js
+const getLogin = (req, res) => {
+	res.render("login");
+};
+
 const getHomepage = (req, res) => {
 	res.render("container", { view: "homepage" });
 };
@@ -15,4 +19,20 @@ const getProfilePage = (req, res) => {
 	res.render("container", { view: "profile" });
 };
 
-module.exports = { getHomepage, getArtistsPage, getTracksPage, getProfilePage };
+const postRegister = (req, res) => {
+	res.send("Created!");
+};
+
+const postLogin = (req, res) => {
+	res.send("Logged in!");
+};
+
+module.exports = {
+	getHomepage,
+	getArtistsPage,
+	getTracksPage,
+	getProfilePage,
+	getLogin,
+	postRegister,
+	postLogin,
+};
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,14 +1,30 @@
 // Điều hướng trang
 const express = require("express");
 const router = express.Router();
-const { getHomepage, getArtistsPage, getTracksPage, getProfilePage } = require("../controllers/home.controller");
+
+// prettier-ignore
+const {
+    getHomepage,
+    getArtistsPage,
+    getTracksPage,
+    getProfilePage,
+    getLogin,
+    postRegister,
+    postLogin
+} = require("../controllers/home.controller");
 
 router.get("/", getHomepage);
+
+router.get("/login", getLogin);
 
 router.get("/artists", getArtistsPage);
 
 router.get("/tracks", getTracksPage);
 
 router.get("/profile", getProfilePage);
+
+router.post("/register", postRegister);
+
+router.post("/login", postLogin);
 
 module.exports = router;
