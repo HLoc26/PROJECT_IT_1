b04c5c294b6d7b8e856f1b0d76a6ad082231fce2
HLoc26 <dhl26052004@gmail.com>
2024-10-16 17:54:12 +0700

    Using knex and check for db connection

--- src/server.js
+++ src/server.js
@@ -2,8 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
-const { ensureConnection } = require("./config/database");
-
+const knex = require("./config/knex");
 const app = express();
 const port = process.env.PORT || 8080;
 const hostname = process.env.HOST_NAME;
@@ -13,6 +12,11 @@ configViewEngine(app);
 
 // Route definition for 127.0.0.1/
 app.use("/", webRoutes);
+
+// Kiểm tra kết nối
+knex.raw("SELECT 1+1 AS result")
+	.then(() => console.log("Database connection is working!"))
+	.catch((err) => console.error("Database connection failed!", err));
 
 // Start the server
 app.listen(port, hostname, () => {
