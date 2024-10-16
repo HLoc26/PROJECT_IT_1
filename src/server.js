af9a2777f39f91dda03fb9cfdcb705b54fa9b92e
HLoc26 <dhl26052004@gmail.com>
2024-10-16 10:38:11 +0700

    removed sample SQL query

--- src/server.js
+++ src/server.js
@@ -14,20 +14,6 @@ configViewEngine(app);
 // Route definition for 127.0.0.1/
 app.use("/", webRoutes);
 
-// Use an async function to handle database queries
-const queryDatabase = async () => {
-	try {
-		const pool = await ensureConnection();
-		const result = await pool.request().query("SELECT * FROM test");
-		console.log(result.recordset);
-	} catch (err) {
-		console.error("Error executing query: ", err);
-	}
-};
-
-// Call the async function to query the database
-queryDatabase();
-
 // Start the server
 app.listen(port, hostname, () => {
 	console.log(`App listening on ${hostname}:${port}`);
