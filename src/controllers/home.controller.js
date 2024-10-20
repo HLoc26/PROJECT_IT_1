f9ebebcde5438d28769016cda608edd319080de1
HLoc26 <dhl26052004@gmail.com>
2024-10-20 19:16:54 +0700

    Update route handler for login

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,4 +1,4 @@
-import { hash } from "bcrypt";
+import { compare, hash } from "bcrypt";
 import knex from "../config/knex.js";
 
 // Các hàm trả về dữ liệu cho routes/web.js
@@ -25,12 +25,6 @@ const getProfilePage = (req, res) => {
 const postRegister = async (req, res) => {
 	const { user_name, user_email, user_password } = req.body;
 	try {
-		// console.log(user_name, user_email, user_password);
-		// Check if email is undefined
-		if (!user_email) {
-			console.log("Email is required");
-			return res.status(400).json({ error: "Email is required" });
-		}
 		// Query
 		const existing = await knex("users").select("user_email").where("user_email", user_email);
 		console.log("Existing:", existing);
@@ -50,15 +44,36 @@ const postRegister = async (req, res) => {
 			user_role: "user",
 		});
 		console.log("Created!"); // TO-DO: Show success alert
-		res.status(201).json({ message: "Account created successfully!" });
+		return res.status(201).json({ message: "Account created successfully!" });
 	} catch (error) {
 		console.error(error);
-		res.status(500).json({ error: "Internal server error" });
+		return res.status(500).json({ error: "Internal server error" });
 	}
 };
 
-const postLogin = (req, res) => {
-	res.send("Logged in!");
+const postLogin = async (req, res) => {
+	// console.log(req.body); // Debug
+	const { login_email, login_password } = req.body;
+
+	try {
+		const user = await knex("users").where({ user_email: login_email }).first();
+		// console.log(user); // Debug
+		if (!user) {
+			console.log("Wro ng email");
+			return res.status(401).json({ error: "Wrong email or password!" });
+		}
+
+		const match = await compare(login_password, user.user_password);
+		if (!match) {
+			console.log("Wrong pwd");
+			return res.status(401).json({ error: "Wrong email or password" });
+		}
+
+		return res.status(200).json({ message: "Login success!" });
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({ error: "Internal server error" });
+	}
 };
 
 export default {
