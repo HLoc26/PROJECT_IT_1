628011c21ecb19300343ed1ba542e6494be27d28
HLoc26 <dhl26052004@gmail.com>
2024-11-04 04:31:37 +0700

    getting artist's information (album, tracks, name)

--- src/models/Artist.js
+++ src/models/Artist.js
@@ -12,12 +12,18 @@ class Artist {
 	static async getArtistTracks(artist_id) {
 		const songs = await knex("tracks as t")
 			.join("artist_perform as ap", "t.track_id", "ap.track_id") // Join artist_perform
-			.join("artists as a", "ap.artist_id", "a.artist_id");
+			.join("artists as a", "ap.artist_id", "a.artist_id")
+			.where("a.artist_id", artist_id);
 		return songs;
 	}
 
+	static async getArtistAlbums(artist_id) {
+		const albums = await knex("albums").select("*").where("artist_id", artist_id);
+		return albums;
+	}
+
 	static async getArtistInfo(artist_id) {
-		const artist = await knex("artists").select("*").where("artist_id", artist_id);
+		const artist = await knex("artists").select("*").where("artist_id", artist_id).first();
 		return artist;
 	}
 }
