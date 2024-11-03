3471ee029a568f603936cf34454023c606898ab9
HLoc26 <dhl26052004@gmail.com>
2024-11-03 17:37:47 +0700

    get track's artist pic if not in any album

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,16 +1,18 @@
 import { Router } from "express";
 import Album from "../models/Album.js";
+import Artist from "../models/Artist.js";
 
 const router = Router();
 // Example of an Express.js endpoint
-router.get("/album/:title", async (req, res) => {
+router.get("/track/img/:title", async (req, res) => {
 	const trackTitle = req.params.title;
 	// console.log("/api/album/:title >>>", trackTitle);
 	try {
-		// Fetch album information from your database (pseudo-code)
-		const album = await Album.getAlbum(trackTitle); // Replace with your actual DB query
-		if (!album) {
-			return res.status(404).send("Album not found");
+		const album = await Album.getAlbum(trackTitle);
+		const track = await Artist.getArtistInfoByTrack(trackTitle);
+		if (!album.album_id) {
+			res.json(track);
+			return;
 		}
 		res.json(album); // Send album data as JSON
 	} catch (error) {
