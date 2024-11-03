e73e5cacfff1a186d9049ea25926eab8da054bdf
HLoc26 <dhl26052004@gmail.com>
2024-11-04 04:32:56 +0700

    add playtrack for song links

--- src/public/js/container.script.js
+++ src/public/js/container.script.js
@@ -1,22 +1,37 @@
-document.addEventListener("DOMContentLoaded", () => {
-	const links = document.querySelectorAll("a.side-link");
-	const content = document.getElementById("content");
+// document.addEventListener("DOMContentLoaded", () => {
+// 	const links = document.querySelectorAll("a.side-link");
+// 	const content = document.getElementById("content");
 
-	links.forEach((link) => {
-		link.addEventListener("click", (e) => {
-			e.preventDefault();
-			const url = e.target.href;
-			history.pushState(null, "", url);
-			// Fetch nội dung của trang mới mà không làm mới trang
-			fetch(url)
-				.then((response) => response.text())
-				.then((html) => {
-					// Lấy nội dung cần thay đổi từ server
-					const newContent = new DOMParser().parseFromString(html, "text/html").querySelector("#content").innerHTML;
-					content.innerHTML = newContent;
-					initHomepage();
-				})
-				.catch((err) => console.error("Error:", err));
-		});
+// 	links.forEach((link) => {
+// 		link.addEventListener("click", (e) => {
+// 			e.preventDefault();
+// 			const url = e.target.href;
+// 			history.pushState(null, "", url);
+// 			// Fetch nội dung của trang mới mà không làm mới trang
+// 			fetch(url)
+// 				.then((response) => response.text())
+// 				.then((html) => {
+// 					// Lấy nội dung cần thay đổi từ server
+// 					const newContent = new DOMParser().parseFromString(html, "text/html").querySelector("#content").innerHTML;
+// 					content.innerHTML = newContent;
+// 					initHomepage();
+// 				})
+// 				.catch((err) => console.error("Error:", err));
+// 		});
+// 	});
+// });
+
+// Set links for song-links
+const songLinks = document.querySelectorAll(".song-link");
+
+songLinks.forEach((link) => {
+	link.addEventListener("click", function (e) {
+		e.preventDefault();
+
+		const mp3Path = this.getAttribute("data-mp3-path");
+		const title = this.getAttribute("data-title");
+		const artist = this.getAttribute("data-artist");
+
+		playTrack(mp3Path, title, artist);
 	});
 });
