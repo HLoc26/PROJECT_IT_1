afeb97677b6a005ad08887ed176563db45c5e361
HLoc26 <dhl26052004@gmail.com>
2024-11-04 20:55:58 +0700

    remove console log

--- src/controllers/artists.controller.js
+++ src/controllers/artists.controller.js
@@ -1,7 +1,6 @@
 import Artist from "../models/Artist.js";
 export async function getArtistsPage(req, res) {
 	const artists = await Artist.getArtists();
-	console.log(">>>>", artists);
 	res.render("vwArtists/artists", { artists: artists });
 }
 
