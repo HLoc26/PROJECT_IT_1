81312f5e16c01d81f0180864c8f7d6b0a5055606
HLoc26 <dhl26052004@gmail.com>
2024-11-09 14:56:22 +0700

    using services to interact with db

--- src/controllers/default.controller.js
+++ src/controllers/default.controller.js
@@ -1,14 +1,16 @@
 import { compare, hash } from "bcrypt";
-import knex from "../config/knex.js";
+import artistService from "../services/artists.service.js";
+import userService from "../services/users.service.js";
 
 export default {
 	// Các hàm trả về dữ liệu cho routes/web.js
 	getLogin(req, res) {
 		res.render("layouts/login", { layout: false });
 	},
+
 	async getHomepage(req, res) {
 		const username = req.session.username;
-		const artists = await Artist.get10Artists();
+		const artists = await artistService.findTop10();
 		res.render("homepage", { artists: artists, username: username });
 	},
 
@@ -16,7 +18,7 @@ export default {
 		res.render("profile");
 	},
 
-	getLogout(req, res) {
+	async getLogout(req, res) {
 		req.session.destroy(function (err) {
 			if (err) {
 				return res.send("Error logging out");
@@ -30,7 +32,7 @@ export default {
 		const { user_name, user_email, user_password } = req.body;
 		try {
 			// Query
-			const existing = await knex("users").select("user_email").where("user_email", user_email);
+			const existing = await userService.findByEmail(user_email);
 			console.log("Existing:", existing);
 			if (existing.length) {
 				console.log("Email in use");
@@ -40,14 +42,16 @@ export default {
 
 			console.log("HashedPWD:", hashedPwd);
 
-			await knex("users").insert({
-				user_name,
+			const new_user = {
+				user_name: user_name,
 				user_password: hashedPwd,
-				user_email,
-				user_profile_pic: null,
+				user_email: user_email,
 				user_role: "user",
-			});
-			console.log("Created!"); // TO-DO: Show success alert
+			};
+
+			const ret = await userService.add(new_user);
+
+			console.log(`Created ${ret} row`); // TO-DO: Show success alert
 			return res.status(201).json({ message: "Account created successfully!" });
 		} catch (error) {
 			console.error(error);
@@ -60,13 +64,13 @@ export default {
 		const { login_email, login_password } = req.body;
 
 		try {
-			const user = await knex("users").where({ user_email: login_email }).first();
+			const users = await userService.findByEmail(login_email);
 			// console.log(user); // Debug
-			if (!user) {
+			if (users.length === 0) {
 				console.log("Wrong email");
 				return res.status(401).json({ error: "Wrong email or password!" });
 			}
-
+			const user = users[0];
 			const match = await compare(login_password, user.user_password);
 			if (!match) {
 				console.log("Wrong pwd");
@@ -77,6 +81,7 @@ export default {
 			req.session.username = user.user_name;
 			req.session.role = user.user_role;
 			req.session.isAuthentication = true;
+
 			console.log("Login OK");
 			return res.status(200).redirect("/home");
 		} catch (error) {
