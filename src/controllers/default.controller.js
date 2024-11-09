5bb740d587df8e47594447d65c78bd759c52e740
HLoc26 <dhl26052004@gmail.com>
2024-11-09 14:34:17 +0700

    renamed from main.controller.js

---
+++ src/controllers/default.controller.js
@@ -0,0 +1,87 @@
+import { compare, hash } from "bcrypt";
+import knex from "../config/knex.js";
+
+export default {
+	// Các hàm trả về dữ liệu cho routes/web.js
+	getLogin(req, res) {
+		res.render("layouts/login", { layout: false });
+	},
+	async getHomepage(req, res) {
+		const username = req.session.username;
+		const artists = await Artist.get10Artists();
+		res.render("homepage", { artists: artists, username: username });
+	},
+
+	getProfilePage(req, res) {
+		res.render("profile");
+	},
+
+	getLogout(req, res) {
+		req.session.destroy(function (err) {
+			if (err) {
+				return res.send("Error logging out");
+			}
+			console.log("Logged out");
+			res.render("layouts/login", { layout: false });
+		});
+	},
+
+	async postRegister(req, res) {
+		const { user_name, user_email, user_password } = req.body;
+		try {
+			// Query
+			const existing = await knex("users").select("user_email").where("user_email", user_email);
+			console.log("Existing:", existing);
+			if (existing.length) {
+				console.log("Email in use");
+				return res.status(400).json({ error: "Email already in use" });
+			}
+			const hashedPwd = await hash(user_password, 10);
+
+			console.log("HashedPWD:", hashedPwd);
+
+			await knex("users").insert({
+				user_name,
+				user_password: hashedPwd,
+				user_email,
+				user_profile_pic: null,
+				user_role: "user",
+			});
+			console.log("Created!"); // TO-DO: Show success alert
+			return res.status(201).json({ message: "Account created successfully!" });
+		} catch (error) {
+			console.error(error);
+			return res.status(500).json({ error: "Internal server error" });
+		}
+	},
+
+	async postLogin(req, res) {
+		// console.log(req.body); // Debug
+		const { login_email, login_password } = req.body;
+
+		try {
+			const user = await knex("users").where({ user_email: login_email }).first();
+			// console.log(user); // Debug
+			if (!user) {
+				console.log("Wrong email");
+				return res.status(401).json({ error: "Wrong email or password!" });
+			}
+
+			const match = await compare(login_password, user.user_password);
+			if (!match) {
+				console.log("Wrong pwd");
+				return res.status(401).json({ error: "Wrong email or password" });
+			}
+
+			req.session.userId = user.user_id;
+			req.session.username = user.user_name;
+			req.session.role = user.user_role;
+			req.session.isAuthentication = true;
+			console.log("Login OK");
+			return res.status(200).redirect("/home");
+		} catch (error) {
+			console.log(error);
+			return res.status(500).json({ error: "Internal server error" });
+		}
+	},
+};
