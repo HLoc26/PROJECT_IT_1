92f6a9d8d4df113c955c46f77a8e199b5723a7d6
HLoc26 <dhl26052004@gmail.com>
2024-11-10 11:11:34 +0700

    force client to login after server reset

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,7 +1,14 @@
+// Middleware to check authentication
 export function isAuthenticated(req, res, next) {
 	if (req.session && req.session.userId) {
 		return next();
 	} else {
-		return res.redirect("/login");
+		console.log("Redirect to login");
+		// Check if request is an AJAX/fetch request
+		if (req.xhr || req.headers["x-requested-with"] === "XMLHttpRequest") {
+			return res.status(303).send("/login"); // Send the login URL without a redirect
+		} else {
+			return res.redirect("/login"); // Regular redirect for non-AJAX requests
+		}
 	}
 }
