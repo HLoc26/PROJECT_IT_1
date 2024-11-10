a61495cd559e5b36cf6b662ef74dc9b4b613e26b
HLoc26 <dhl26052004@gmail.com>
2024-11-11 00:30:30 +0700

    handling [POST] /tracks/upload to upload file

--- src/routes/tracks.routes.js
+++ src/routes/tracks.routes.js
@@ -1,12 +1,29 @@
 import express from "express";
 import tracksController from "../controllers/tracks.controller.js";
 
+import upload from "../config/fileUpload.js";
+import containerClient from "../config/azureStorage.js";
+
 const router = express.Router();
 
 router.get("/", tracksController.getTracksPage);
 
 router.get("/upload", function (req, res) {
 	res.render("vwTracks/upload");
+});
+
+router.post("/upload", upload.single("musicFile"), async function (req, res) {
+	if (!req.file) {
+		return res.status(400).redirect("/tracks/upload");
+	}
+	const blobName = req.file.filename;
+	const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+	try {
+		await blockBlobClient.upload(req.file.buffer, req.file.size);
+
+		const fileUrl = `https://${process.env.AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net/${containerName}/${blobName}`;
+	} catch (error) {}
 });
 
 router.get("/:id", tracksController.getTrackDetail);
