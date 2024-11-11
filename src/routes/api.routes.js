8b8a554622937e148fab4cad2183c2925ac49454
HLoc26 <dhl26052004@gmail.com>
2024-11-11 13:58:47 +0700

    route definition and handler for [GET] /api/tracks/:id

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,8 +1,25 @@
 import { Router } from "express";
 import apiController from "../controllers/api.controller.js";
 
+import trackService from "../services/tracks.service.js";
+import albumService from "../services/albums.service.js";
+import artistService from "../services/artists.service.js";
+
 const router = Router();
-// Example of an Express.js endpoint
-router.get("/track/img/:title", apiController.getAlbumInfo);
+
+// Get information of a track: id, name, mp3 path, duration,...
+router.get("/tracks/:id", async function (req, res) {
+	const track_id = req.params.id;
+
+	const track = await trackService.findById(track_id);
+	const album = await albumService.findByTrackId(track_id);
+	const artist = await artistService.findByTrackId(track_id);
+
+	const ret = { ...track, ...album, ...artist };
+
+	console.log("RET: ", ret);
+
+	res.status(200).json(ret);
+});
 
 export default router;
