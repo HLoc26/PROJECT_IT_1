e051826cf966e0041d511d93d661b9440251aee4
HLoc26 <dhl26052004@gmail.com>
2024-11-16 21:15:39 +0700

    save user id to local

--- src/middlewares/setUser.js
+++ src/middlewares/setUser.js
@@ -1,8 +1,10 @@
 export function setUsername(req, res, next) {
 	if (req.session && req.session.username) {
 		res.locals.username = req.session.username;
+		res.locals.uid = req.session.userId;
 	} else {
 		res.locals.username = null;
+		res.locals.uid = null;
 	}
 	next();
 }
