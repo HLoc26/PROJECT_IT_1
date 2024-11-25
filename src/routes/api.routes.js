773de4e61fce0b67b6cec0e8847f1ba77566cd13
HLoc26 <dhl26052004@gmail.com>
2024-11-25 18:16:30 +0700

    route definition for [POST] /api/history and [GET] /api/session

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -6,4 +6,8 @@ const router = Router();
 // Get information of a track: id, name, mp3 path, duration,...
 router.get("/tracks/:id", apiController.getTrackInfo);
 
+router.post("/history", apiController.addHistory);
+
+router.get("/session", apiController.getSession);
+
 export default router;
