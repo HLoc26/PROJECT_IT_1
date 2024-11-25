bd9600a40a8ed8b7809ed1771da5fe1dcf0c275a
HLoc26 <dhl26052004@gmail.com>
2024-11-25 18:15:22 +0700

    history and session handler

--- src/controllers/api.controller.js
+++ src/controllers/api.controller.js
@@ -1,6 +1,7 @@
 import trackService from "../services/tracks.service.js";
 import albumService from "../services/albums.service.js";
 import artistService from "../services/artists.service.js";
+import historyService from "../services/history.service.js";
 
 export default {
 	async getTrackInfo(req, res) {
@@ -12,8 +13,34 @@ export default {
 
 		const ret = { ...track, ...album, ...artist };
 
-		console.log("RET: ", ret);
+		// console.log("RET: ", ret);
 
 		res.status(200).json(ret);
+	},
+
+	async addHistory(req, res) {
+		console.log("AddHistory: ", req.body);
+		const { user_id, track_id } = req.body;
+		console.log(user_id, track_id);
+		const entity = {
+			user_id: user_id,
+			track_id: track_id,
+			time: new Date().toISOString(),
+		};
+
+		const ret = await historyService.addHistory(entity);
+		console.log(ret);
+		res.status(200).json({ message: "Saved history" });
+	},
+
+	async getSession(req, res) {
+		if (req.session && req.session.user_id) {
+			res.json({
+				user_id: req.session.user_id,
+				username: req.session.username,
+			});
+		} else {
+			res.status(401).json({ message: "User not authenticated" });
+		}
 	},
 };
