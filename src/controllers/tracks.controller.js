1e1507eca39d1bed355aead3d4d65fc8e4511b60
HLoc26 <dhl26052004@gmail.com>
2024-11-27 16:55:44 +0700

    update track metadata handler

--- src/controllers/tracks.controller.js
+++ src/controllers/tracks.controller.js
@@ -2,8 +2,9 @@ import artistService from "../services/artists.service.js";
 import albumService from "../services/albums.service.js";
 import trackService from "../services/tracks.service.js";
 
-import { parseBuffer } from "music-metadata";
-import containerClient from "../config/azureStorage.js";
+import { parseFile } from "music-metadata";
+import path from "path";
+import { fileURLToPath } from "url";
 
 export default {
 	// Controller or Service function to get tracks with artists
@@ -51,20 +52,31 @@ export default {
 		const { upload_track_name, isVisible, upload_lyrics } = req.body;
 		const visibility = isVisible === "on" ? "public" : "private";
 		const lyrics = upload_lyrics && upload_lyrics.trim() ? upload_lyrics : null;
-
+		const uploader_id = res.locals.user_id;
 		try {
+			// Get the current module's directory path
+			const __filename = fileURLToPath(import.meta.url);
+			const __dirname = path.dirname(__filename);
+
+			// Construct full path to the saved file
+			const fullFilePath = path.join(__dirname, "..", "public", "uploads", "music", req.file.filename);
+
+			console.log(fullFilePath);
+
 			// track_duration
-			const metadata = await parseBuffer(req.file.buffer);
-			const duration = metadata.format.duration;
+			const metadata = await parseFile(fullFilePath);
+			console.log(metadata);
+			const duration = Math.floor(metadata.format.duration);
 
 			// track_mp3_path (local path)
-			const filePath = `/uploads/music/${req.file.filename}`;
+			const filePath = req.file.filename;
 
 			// Insert to db
 			const entity = {
 				track_title: upload_track_name,
 				track_duration: duration,
 				track_mp3_path: filePath, // Local path
+				uploader_id: uploader_id,
 				lyrics: lyrics,
 				visibility: visibility,
 			};
