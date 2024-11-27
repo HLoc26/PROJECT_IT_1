2bf53922240ca8b56465324b33a4a66e5d12c106
HLoc26 <dhl26052004@gmail.com>
2024-11-27 14:49:04 +0700

    add liked parameter for views

--- src/controllers/artists.controller.js
+++ src/controllers/artists.controller.js
@@ -1,6 +1,7 @@
 import artistService from "../services/artists.service.js";
 import albumService from "../services/albums.service.js";
 import trackService from "../services/tracks.service.js";
+import likeService from "../services/like.service.js";
 
 export default {
 	async getArtistsPage(req, res) {
@@ -15,6 +16,13 @@ export default {
 		const artist_albums = await albumService.findByArtistId(artist_id); // Artists albums
 		const artist_tracks = await trackService.findByArtistId(artist_id); // Artists tracks
 
-		return res.render("vwArtists/artist_home", { artist: artist, albums: artist_albums, tracks: artist_tracks });
+		const liked = await likeService.findLikedArtist(res.locals.user_id, artist_id);
+
+		return res.render("vwArtists/artist_detail", {
+			artist: artist,
+			albums: artist_albums,
+			tracks: artist_tracks,
+			liked: liked,
+		});
 	},
 };
