507888153ea5fcecf9d3fd9ab0184ea832fd9cec
HLoc26 <dhl26052004@gmail.com>
2024-11-27 13:23:20 +0700

    unlike

--- src/services/like.service.js
+++ src/services/like.service.js
@@ -23,4 +23,24 @@ export default {
 		const entity = { user_id: user_id, album_id: album_id, time: time };
 		return db("user_like_album").insert(entity);
 	},
+
+	// Remove from user_like_track
+	unlikeTrack(user_id, track_id) {
+		return db("user_like_track").where({ user_id: user_id, track_id: track_id }).del();
+	},
+
+	// Remove from user_like_artist
+	unlikeArtist(user_id, artist_id) {
+		return db("user_like_artist").where({ user_id: user_id, artist_id: artist_id }).del();
+	},
+
+	// Remove from user_like_playlist
+	unlikePlaylist(user_id, playlist_id, time) {
+		return db("user_like_playlist").where({ user_id: user_id, playlist_id: playlist_id }).del();
+	},
+
+	// Remove from user_like_album
+	unlikeAlbum(user_id, album_id, time) {
+		return db("user_like_album").where({ user_id: user_id, album_id: album_id }).del();
+	},
 };
