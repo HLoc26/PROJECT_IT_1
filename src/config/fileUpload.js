438cd059c743c52340601ad57092d9519d525f18
HLoc26 <dhl26052004@gmail.com>
2024-11-27 16:55:20 +0700

    update path to music folder

--- src/config/fileUpload.js
+++ src/config/fileUpload.js
@@ -1,9 +1,13 @@
 import multer from "multer";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const storage = multer.diskStorage({
 	destination: (req, file, cb) => {
-		cb(null, path.join(__dirname, "../../public/uploads/musics"));
+		cb(null, path.join(__dirname, "../public/uploads/music"));
 	},
 	filename: (req, file, cb) => {
 		const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
