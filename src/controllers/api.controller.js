7089aa3e13bb0710b847e079d97a8b7ec878a5d8
HLoc26 <dhl26052004@gmail.com>
2024-11-27 14:24:47 +0700

    controller for like and unlike

--- src/controllers/api.controller.js
+++ src/controllers/api.controller.js
@@ -2,7 +2,7 @@ import trackService from "../services/tracks.service.js";
 import albumService from "../services/albums.service.js";
 import artistService from "../services/artists.service.js";
 import historyService from "../services/history.service.js";
-
+import likeService from "../services/like.service.js";
 export default {
 	async getTrackInfo(req, res) {
 		const track_id = req.params.id;
@@ -41,6 +41,61 @@ export default {
 			});
 		} else {
 			res.status(401).json({ message: "User not authenticated" });
+		}
+	},
+
+	async like(req, res) {
+		// console.log("like:", req.body);
+		const { type, id } = req.body;
+		const user_id = res.locals.user_id;
+		const time = new Date().toISOString();
+		try {
+			switch (type) {
+				case "track":
+					await likeService.likeTrack(user_id, id, time);
+					break;
+				case "artist":
+					await likeService.likeArtist(user_id, id, time);
+					break;
+				case "playlist":
+					await likeService.likePlaylist(user_id, id, time);
+					break;
+				case "album":
+					await likeService.likeAlbum(user_id, id, time);
+					break;
+				default:
+					break;
+			}
+			return res.status(200).json({ message: "Success" });
+		} catch (error) {
+			return res.status(400).json({ message: error });
+		}
+	},
+
+	async unlike(req, res) {
+		// console.log("unlike:", req.body);
+		const { type, id } = req.body;
+		const user_id = res.locals.user_id;
+		try {
+			switch (type) {
+				case "track":
+					await likeService.unlikeTrack(user_id, id);
+					break;
+				case "artist":
+					await likeService.unlikeArtist(user_id, id);
+					break;
+				case "playlist":
+					await likeService.unlikePlaylist(user_id, id);
+					break;
+				case "album":
+					await likeService.unlikeAlbum(user_id, id);
+					break;
+				default:
+					break;
+			}
+			return res.status(200).json({ message: "Success" });
+		} catch (error) {
+			return res.status(400).json({ message: error });
 		}
 	},
 };
