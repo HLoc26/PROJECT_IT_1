2ce48aa9032d72a567f26055fb9967039bdd36f8
HLoc26 <dhl26052004@gmail.com>
2024-12-02 02:29:37 +0700

    api route definition for getting playlists and save track to playlists

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import apiController from "../controllers/api.controller.js";
+import playlistService from "../services/playlists.service.js";
 
 const router = Router();
 
 // Get information of a track: id, name, mp3 path, duration,...
 router.get("/tracks/:id", apiController.getTrackInfo);
+
+router.get("/playlists", apiController.getPlaylists);
+
+router.post("/save-track-playlists", apiController.saveTrackPlaylists);
 
 router.post("/history", apiController.addHistory);
 
