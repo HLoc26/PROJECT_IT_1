e45315f8a6e115101b03b96ede35b1de62578835
HLoc26 <dhl26052004@gmail.com>
2024-12-03 16:25:26 +0700

    showing public playlists

--- src/controllers/default.controller.js
+++ src/controllers/default.controller.js
@@ -5,6 +5,7 @@ import tracksService from "../services/tracks.service.js";
 import albumService from "../services/albums.service.js";
 import historyService from "../services/history.service.js";
 import likeService from "../services/like.service.js";
+import playlistsService from "../services/playlists.service.js";
 
 export default {
 	// Các hàm trả về dữ liệu cho routes/web.js
@@ -81,11 +82,30 @@ export default {
 			})
 		);
 
+		// Get user's playlists
+		const user_playlists = await playlistsService.findPublicByUserId(req.session.user_id);
+
+		// Function to get playlist cover
+		async function getPlaylistCover(playlistId) {
+			const tracks = await tracksService.findByPlaylistId(playlistId);
+			const firstTrack = tracks[0];
+			// console.log(playlistId, firstTrack);
+			if (!firstTrack) return "/images/albums/album_default.png";
+
+			const album = await albumService.findById(firstTrack.album_id);
+			return album && album.album_cover_image ? album.album_cover_image : "/images/albums/album_default.png";
+		}
+
+		// Add cover images to created playlists
+		for (const playlist of user_playlists) {
+			playlist.cover = await getPlaylistCover(playlist.playlist_id);
+		}
+
 		const liked_artists = await likeService.findLikedArtists(user_id);
 		// console.log(liked_artists);
 		// console.log(track_history);
 
-		res.render("vwProfile/my_profile", { user: user, track_history: track_history, liked_artists: liked_artists });
+		res.render("vwProfile/my_profile", { user: user, track_history: track_history, playlists: user_playlists, liked_artists: liked_artists });
 	},
 
 	async getLogout(req, res) {
